fecfe3560023406db04119fd2301230ec4a816ea
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 09:56:11 -0400

    init

---
+++ src/average_temperature.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def average_temperature():
+    pass
+
+def main():
+    result = average_temperature()
+    print(f"Average temperature in July: {result:.1f}")
+
+if __name__ == "__main__":
+    main()
